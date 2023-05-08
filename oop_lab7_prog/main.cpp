07de83969a0f327fc84e01b52d7711dce6f48d71
Lesyk222 <oleszherebetskyi@gmail.com>
2023-05-08 14:34:02 +0300

    added final version

--- oop_lab7_prog/main.cpp
+++ oop_lab7_prog/main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-void GetMinMaxBills(unique_ptr<CElectricityConsumption>* arr, int size, double dConsumptionPerHours[24U], double& minBill, double& maxBill)
+void GetMinMaxBills(CElectricityConsumption** arr, int size, double dConsumptionPerHours[24U], double& minBill, double& maxBill)
 {
     minBill = arr[0]->GetElectricityBill(dConsumptionPerHours);
     maxBill = minBill;
@@ -30,54 +30,41 @@ int main()
 {
     double dConsumption[24] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24 };
 
-    //unique_ptr<CElectricityConsumption> consumption1 = make_unique<CFixedElectricityConsumption>();
-    //unique_ptr<CElectricityConsumption> consumption2 = make_unique<CSocialElectricityConsumption>();
-    //unique_ptr<CElectricityConsumption> consumption3 = make_unique<CMultiZoneElectricityConsumption>();
-
-    //double bill1 = consumption1->GetElectricityBill(dConsumption);
-    //double bill2 = consumption2->GetElectricityBill(dConsumption);
-    //double bill3 = consumption3->GetElectricityBill(dConsumption);
-
-    CElectricityConsumption* consumption = nullptr;
 
     cout << "Choose the tariff (1 - Fixed, 2 - Social, 3 - Multi-zone): ";
     int choice;
     cin >> choice;
+    CElectricityConsumption* consumption = nullptr;
 
- /*   switch (choice) {
+    switch (choice) {
     case 1:
-        consumption = dynamic_cast<CFixedElectricityConsumption*>(consumption1.get());
+        consumption = new CFixedElectricityConsumption();
         break;
     case 2:
-        consumption = dynamic_cast<CSocialElectricityConsumption*>(consumption2.get());
+        consumption = new CSocialElectricityConsumption();
         break;
     case 3:
-        consumption = dynamic_cast<CMultiZoneElectricityConsumption*>(consumption3.get());
+        consumption = new CMultiZoneElectricityConsumption();
         break;
     default:
         cout << "Invalid choice!" << endl;
-    }*/
+        return 1;
+    }
 
-   switch (choice) {
-   case 1:
-       consumption = new CFixedElectricityConsumption();
-       //consumption = dynamic_cast<CFixedElectricityConsumption*>();
-       break;
-   case 2:
-       consumption = new CSocialElectricityConsumption();
-       break;
-   case 3:
-       consumption = new CMultiZoneElectricityConsumption();
-       break;
-   default:
-       cout << "Invalid choice!" << endl;
-   }
+    if (typeid(*consumption) == typeid(CFixedElectricityConsumption)) {
+        double selectedBill = consumption->GetElectricityBill(dConsumption);
+        cout << "Choosed fixed price: " << selectedBill << endl << endl;  
+    }
+    else if (typeid(*consumption) == typeid(CSocialElectricityConsumption)) {
+        double selectedBill = consumption->GetElectricityBill(dConsumption);
+        cout << "Choosed social price: " << selectedBill << endl << endl;
+    }
+    else if (typeid(*consumption) == typeid(CMultiZoneElectricityConsumption)) {
+        double selectedBill = consumption->GetElectricityBill(dConsumption);
+        cout << "Choosed multizone: " << selectedBill << endl << endl;
+    }
 
-    double selectedBill = consumption->GetElectricityBill(dConsumption);
-
-    cout << "Choosed price: " << selectedBill << endl
-        << typeid(consumption).name() << endl;
-    consumption->PrintName();
+    delete consumption;
 
     return 0;
 }
